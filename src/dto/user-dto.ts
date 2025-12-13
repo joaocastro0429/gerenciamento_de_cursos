2102c97faf4eb3915ec62a5bc703854a680986c1
joaocastro0429 <joaocastro0429@gmail.com>
2025-12-13 13:56:12 -0300

    add validação para o nest

--- src/dto/user-dto.ts
+++ src/dto/user-dto.ts
@@ -1,7 +1,18 @@
+import { IsDate, IsNumber, IsString } from "class-validator";
+
 export class UserDto {
+  @IsString()
   id: string;
+
+  @IsString()
   name: string;
+
+  @IsString()
   description: string;
+
+  @IsNumber()
   duration: number;
+
+  @IsDate()
   date_init: Date;
 }
